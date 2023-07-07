2e733133118f16c3fa9a54290fe94bd3f0bc05c4
Santosh Jadhav <ankushjadhavdotnetcore2023@gmail.com>
2023-07-07 17:14:19 +0530

    Calculation Class added

--- AkashDemo/Program.cs
+++ AkashDemo/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AkashDemo;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter first Number");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter second Number");
+        int b = int.Parse(Console.ReadLine());
+
+        Calculation calculation = new Calculation();
+        calculation.Add(a,b);
+
+        Console.ReadLine();
+    }
+}
